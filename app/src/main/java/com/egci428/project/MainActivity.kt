f40c3028add41282c683f5bf6d021c062af8a727
bambamm <pimmat.cht@student.mahidol.edu>
2024-11-21 23:25:34 +0700

    second commit

--- app/src/main/java/com/egci428/project/MainActivity.kt
+++ app/src/main/java/com/egci428/project/MainActivity.kt
@@ -8,10 +8,8 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.firebase.Firebase
-import com.google.firebase.firestore.firestore
 
 class MainActivity : AppCompatActivity() {
-    val db = Firebase.firestore
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -22,22 +20,5 @@ class MainActivity : AppCompatActivity() {
             insets
         }
     }
-    private fun submitData() {
 
-
-        var db = dataReference.collection("menuCollection")
-        val messageId = db.document().id
-        val messageData = Message(messageId, msg, ratingBar.rating.toInt(), System.currentTimeMillis().toString())
-
-        db.add(messageData)
-            .addOnSuccessListener {
-                Toast.makeText(applicationContext, "Message is saved successfully", Toast.LENGTH_SHORT).show()
-            }
-            .addOnFailureListener{
-                Toast.makeText(applicationContext, "Fail to save message", Toast.LENGTH_SHORT).show()
-            }
-
-        Log.d("testadd", msg)
-
-    }
 }
